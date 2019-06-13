c74cfcfa89c5f67c0c354198dcec45e92753ca31
Stephen Chen <chenxistephen@gmail.com>
2019-06-13 06:44:56 +0000

    FashionV3 gn configs

--- configs/FashionV3/train_gn_retina_r50.sh
+++ configs/FashionV3/train_gn_retina_r50.sh
@@ -6,5 +6,5 @@ CUDA_VISIBLE_DEVICES="$GPUS"
 python tools/train_net.py \
     --multi-gpu-testing \
     --cfg configs/FashionV3/retinanet_R-50-FPN_2x_gn.yaml \
-    OUTPUT_DIR ./detectron-output \
+    OUTPUT_DIR ./detectron-output/FashionV3_gn \
     #USE_NCCL True
