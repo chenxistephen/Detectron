fa316895b2759e4a030c4a93c614e994e3a9f192
Stephen Chen <chenxistephen@gmail.com>
2019-10-02 07:02:29 +0000

    add init.sh for DLTS

---
+++ init.sh
@@ -0,0 +1,2 @@
+sudo pip install --user future numpy protobuf typing hypothesis networkx opencv-python matplotlib && sudo pip install -r requirements.txt && make && python ./detectron/tests/test_spatial_narrow_as_op.py
+
